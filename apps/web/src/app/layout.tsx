7e1be6e134a625a096119454a5267f5bc9147b15
DimitriGilbert <dimitri.gilbert@gmail.com>
2025-07-04 19:33:12 +0200

    counter

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
           <main>{children}</main>
         </ThemeProvider>
       </body>
+      <script src="https://cdn.counter.dev/script.js" data-id="154c6878-7558-4eff-90f9-bd4904015df1" data-utcoffset="1"></script>
     </html>
   )
 }
