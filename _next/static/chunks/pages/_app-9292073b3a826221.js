5e2e64f902cfe96369c3880bc71f77e201690fa8
DimitriGilbert <dimitri.gilbert@gmail.com>
2026-03-04 00:35:04 +0100

    Updates

---
+++ _next/static/chunks/pages/_app-9292073b3a826221.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{74506:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(77437)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,411],()=>(n(74506),n(50527))),_N_E=_.O()}]);
